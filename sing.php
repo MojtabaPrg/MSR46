d95416a7fcd3246c612f8eb4282fef59bbdbc96f
MojtabaPrg <prgmojtaba@gmail.com>
2025-02-07 14:16:29 +0400

    Delete sing.php

--- sing.php
+++
@@ -1,8 +0,0 @@
-<?php
-$User=$_POST["user"];
-$Email=$_POST["Email"];
-$Password=$_POST["pass"];
-$s=mysqli_connect("localhost" , "root" , "" , "sign");
-mysqli_query($s,"INSERT INTO `signup`(`User Name`, `Email`, `Password`) VALUES ('$User','$Email','$Password')");
-mysqli_close($s);
-?>
